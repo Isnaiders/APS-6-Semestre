1e7ceaa6aeb2ce6bd3b8be1639f78ad8fa4031bf
VitorHSO <vhugo.soc@gmail.com>
2022-10-15 18:31:36 -0300

    Implementação do GetImage

---
+++ ProjectWebMVC/Controllers/ImageController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectWebMVC.Controllers
+{
+    public class ImageController : Controller
+    {
+        private string serverPath;
+
+        public ImageController(IWebHostEnvironment system)
+        {
+            serverPath = system.WebRootPath;
+        }
+
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Upload(IFormFile image)
+        {
+            string savePath = serverPath + "\\images\\";
+
+            if (!Directory.Exists(savePath))
+                Directory.CreateDirectory(savePath);
+
+            using (var stream = System.IO.File.Create(savePath + image.FileName))
+                image.CopyToAsync(stream);
+
+            return RedirectToAction("Upload");
+        }
+    }
+}
